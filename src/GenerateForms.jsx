6f7071c82c4b846c534ffd3adab525077818fcac
Kolilia <jwm20002@mail.ru>
2021-07-09 23:01:30 +0400

    init

---
+++ src/GenerateForms.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GenerateForms = () => {
+  return <div></div>;
+};
+
+export default GenerateForms;
---
+++ src/ListForms.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListForms = () => {
+  return <div></div>;
+};
+
+export default ListForms;
